73778f3fef5efba24e8b9216390c3c7154903cee
Vu Do <1433070+dohaivu@users.noreply.github.com>
2026-09-10 11:16:41 +0700

    add firebase storage lib

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -58,6 +58,7 @@ kotlin {
             implementation(libs.firebase.analytics)
             implementation(libs.firebase.crashlytics)
             implementation(libs.firebase.firestore)
+            implementation(libs.firebase.storage)
             implementation(libs.firebase.auth)
             implementation(libs.play.services.auth)
         }
